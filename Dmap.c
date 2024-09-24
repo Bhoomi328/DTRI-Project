bc7a4b0994dfcfd2e39f66c5ccbcb8ec8904a859
Bhoomika <bhoomikamh32@gmail.com>
2024-09-24 20:49:55 +0530

    Dynamic Memory Allocation Programs Added

---
+++ Dmap.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,size,newsize,*ptr;
+    printf("enter the sizeeee:");
+    scanf("%d",&size);
+    ptr = (int*)calloc(size,sizeof(int));
+    printf("enter the elements:");
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",ptr+i);
+    }
+    printf("the elements are:");
+    for(i=0;i<size;i++)
+    {
+        printf("%d",*(ptr+i));
+    }
+    printf("\n");
+    printf("enter the  new size:");
+    scanf("%d",&newsize);
+    ptr = (int*)realloc(ptr,newsize * sizeof(int));
+    printf("enter the new elements:");
+    for(i=size;i<newsize;i++)
+    {
+        scanf("%d",ptr+i);
+    }
+    printf("the elements are:");
+    for(i=0;i<newsize;i++)
+    {
+        printf("%d ",*(ptr+i));
+    }
+    printf("\n");
+    free(ptr);
+    return 0;
+}
+
+
+
